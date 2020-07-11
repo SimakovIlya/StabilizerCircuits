49654111f23ddcc257e3b1eb314aee2310d2bfdf
SimakovIlya <50998922+SimakovIlya@users.noreply.github.com>
2020-07-11 16:25:11 +0300

    add noise

--- simulator/gates.py
+++ simulator/gates.py
@@ -1,26 +1,95 @@
-from .basic_gates import *
+import numpy as np
 
 
 
-def X_gate(tableau, a):
-	'''
-	X_gate (NOT) on qubit a
-	'''
-	if a >= tableau.shape[0]//2:
-		print('Gate error, there is no such qubit!')
-	tableau = Hadamard(tableau, a)
-	tableau = Z_gate(tableau, a)
-	tableau = Hadamard(tableau, a)
+
+
+def X_gate(tableau, q):
+	tableau[:, -1] ^= tableau[:, q + tableau.shape[0]//2]
 	return tableau
 
 
-def Z_gate(tableau, a):
+
+
+def Y_gate(tableau, q):
+	tableau[:, -1] ^= tableau[:, q] ^ tableau[:, q + tableau.shape[0]//2]
+	return tableau
+
+
+
+
+def Z_gate(tableau, q):
+	tableau[:, -1] ^= tableau[:, q]
+	return tableau
+
+
+
+
+
+# def X_gate(tableau, a):
+# 	'''
+# 	X_gate (NOT) on qubit a
+# 	'''
+# 	if a >= tableau.shape[0]//2:
+# 		print('Gate error, there is no such qubit!')
+# 	tableau = Hadamard(tableau, a)
+# 	tableau = Z_gate(tableau, a)
+# 	tableau = Hadamard(tableau, a)
+# 	return tableau
+
+
+# def Z_gate(tableau, a):
+# 	'''
+# 	Z_gate on qubit a
+# 	'''
+# 	if a >= tableau.shape[0]//2:
+# 		print('Gate error, there is no such qubit!')
+# 	return Phase(Phase(tableau, a), a)
+
+
+
+
+def CNOT(tableau, a, b):
 	'''
-	Z_gate on qubit a
+	CNOT from control a to target b
 	'''
-	if a >= tableau.shape[0]//2:
+	n = tableau.shape[0]//2
+	if a >= n or b >= n:
 		print('Gate error, there is no such qubit!')
-	return Phase(Phase(tableau, a), a)
+	tableau[:, 2*n] = (tableau[:, 2*n] + tableau[:, a]*tableau[:, n+b]*(tableau[:, b] + tableau[:, n+a] + 1)%2)%2
+	tableau[:, b] = (tableau[:, b] + tableau[:, a])%2
+	tableau[:, n+a] =  (tableau[:, n+a] + tableau[:, n+b])%2
+	return tableau
+
+
+
+
+def Hadamard(tableau, a):
+	'''
+	Hadamard on qubit a
+	'''
+	n = tableau.shape[0]//2
+	if a >= n:
+		print('Gate error, there is no such qubit!')
+	tableau[:, 2*n] = (tableau[:, 2*n] + tableau[:, a]*tableau[:, n+a])%2
+	tableau[:, [a, n+a]] = tableau[:, [n+a, a]]
+	return tableau
+
+
+
+
+def Phase(tableau, a):
+	'''
+	Phase gate (S-gate) on qubit a
+	'''
+	n = tableau.shape[0]//2
+	if a >= n:
+		print('Gate error, there is no such qubit!')
+	tableau[:, 2*n] = (tableau[:, 2*n] + tableau[:, a]*tableau[:, n+a])%2
+	tableau[:, n+a] = (tableau[:, n+a] + tableau[:, a])%2
+	return tableau
+
+
 
 
 def iSWAP(tableau, a, b):
